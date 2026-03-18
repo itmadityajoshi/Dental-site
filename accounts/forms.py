45212c5083968776d63f59758bba6aee5c4b31f5
Aditya Joshi <adityajoshi316@gmail.com>
2026-03-18 20:46:36 +0100

    user-register solved as login automatically after creating new user, changed in forms.py

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,16 +3,24 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 from accounts.models import CustomUser
 
-class RegistrForm(UserCreationForm):
+class RegisterForm(UserCreationForm):
     email = forms.EmailField(required=False)
     role = forms.ChoiceField(choices=[
-        ('patinet','Patient'),
+        ('patient','Patient'),
         ('doctor','Doctor'),
     ])
 
     class Meta:
         model = CustomUser
         fields = ['username', 'email', 'role', 'password1','password2']
+    
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.role = self.cleaned_data['role']  # ← explicitly save role
+        if commit:
+            user.save()
+        return user
 
 
 class LoginForm(AuthenticationForm):
--- accounts/views.py
+++ accounts/views.py
@@ -1,29 +1,29 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
-from .forms import RegistrForm, LoginForm
+from .forms import RegisterForm, LoginForm
 
 # Create your views here.
 
 def redirect_by_role(user):
     if user.role == 'doctor':
-        return '/account/dashboard'
+        return '/accounts/dashboard'
     elif user.role == 'patient':
-        return '/account/dashboard'
+        return '/accounts/dashboard'
     else: 
         return '/admin'
     
 
 def register_view(req):
     if req.method == 'POST':
-        form = RegistrForm(req.POST)
+        form = RegisterForm(req.POST) #collects the form data
         if form.is_valid():
-            user = form.save()
-            login(req, user)
-            return redirect(redirect_by_role(user))
+            user = form.save()  #create new user in database
+            login(req, user)   #login them automatically
+            return redirect(redirect_by_role(user)) #go to dashboard according to the user's role.
     else:
-        form = RegistrForm()
-    return render(req, 'accounts/register.html',{'form':form})
+        form = RegisterForm()
+    return render(req, 'accounts/register.html', {'form':form})
 
 
 def login_view(req):
@@ -32,6 +32,7 @@ def login_view(req):
         if form.is_valid():
             user = form.get_user()
             login(req, user)
+            return redirect(redirect_by_role(user))
     else:
         form = LoginForm()
     
@@ -43,7 +44,7 @@ def login_view(req):
 
 def logout_view(req):
     logout(req)
-    return redirect ('/account/login/')
+    return redirect ('/accounts/login/')
 
 
 @login_required  #decorators redirects them to the login page, if without being logged in
